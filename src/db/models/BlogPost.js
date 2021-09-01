f372c16aae8efbb3665b3547234cc787caf924e6
Koulinn <drdverzola@gmail.com>
2021-09-01 13:40:58 +0100

    added BlogComments

--- src/db/models/BlogPost.js
+++ src/db/models/BlogPost.js
@@ -1,22 +1,28 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
-const {Schema, model} = mongoose
+const { Schema, model } = mongoose
 
 const blogPostSchema = new Schema({
-    category : {type: String, required: true},
-    title : {type: String, required: true},
-    cover : {type: String, required: true},
-    readTime : {
-        value: {type:Number, required: true},
-        unit: {type:String, required: true}
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String, required: true },
+    readTime: {
+        value: { type: Number, required: true },
+        unit: { type: String, required: true }
     },
-    author : {
-        name: {type:String, required: true},
-        avatar: {type:String, required: true},
+    author: {
+        name: { type: String, required: true },
+        avatar: { type: String, required: true },
     },
-    content: {type:String, required: true},
-   }, {
-       timestamps: true
-   })
+    content: { type: String, required: true },
+    blogComments: [{
+        comment: String,
+        rate: Number,
+    },{
+        timestamps: true
+    }]
+}, {
+    timestamps: true
+})
 
 export default model('BlogPost', blogPostSchema)
